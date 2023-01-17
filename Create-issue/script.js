a929848f84348355f67f7400e60b063ad970afaa
johann <johann.cavallucci@epitech.eu>
2023-01-17 11:18:56 +0100

    [+] - create issue done, with inputs : title, description and label

--- Create-issue/script.js
+++ Create-issue/script.js
@@ -1,60 +1,40 @@
 require('console-stamp')(console, 'yyyy-mm-dd HH:MM:ss');
 require('dotenv').config();
 
-const commander = require("commander");
 const {Octokit} = require("octokit");
+const { Command } = require('commander');
+const program = new Command();
 
-commander
-    .version("1.0.0")
+program
     .usage('[options]')
-    .option('-t, --title <title>', 'Title of the branch')
+    .option('-t, --title <title>', 'Title of the issue')
+    .option('-d, --description <description>', 'Description of the issue')
+    .option('-l, --label <label>', 'Label of the issue')
     .parse(process.argv);
 
-const program = commander.opts();
+const options = program.opts();
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
 });
 
-const getBranches = async () => {
-    const response = await octokit.request('GET /repos/{owner}/{repo}/branches', {
+const createIssue = async () => {
+    console.log("Creating issue");
+    const response = await octokit.request('POST /repos/{owner}/{repo}/issues', {
         owner: process.env.GITHUB_OWNER,
         repo: process.env.GITHUB_REPO,
-    });
-    return response.data;
-}
+        title:  options.title,
+        body: options.description,
+        labels: [options.label]
 
-const checkBranchName = (branchName, branches) => {
-    return branches.find(branch => branch.name === branchName);
-}
-
-const getLatestCommitOnMaster = async (branches) => {
-    const branch = branches.find(branch => branch.name === 'master');
-    const response = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}', {
-        owner: process.env.GITHUB_OWNER,
-        repo: process.env.GITHUB_REPO,
-        ref: branch.commit.sha,
-    });
+    })
+    console.log("Response");
     return response.data;
 }
 
 async function launchScript() {
-    const branchName = program.title;
-    const branches = await getBranches();
-    const branch = checkBranchName(branchName, branches);
-    if (branch) {
-        console.log("Branch already exists");
-        return false;
-    }
-    const latestCommitOnMaster = await getLatestCommitOnMaster(branches);
-    console.log("Creating branch : " + branchName);
-    const response = await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
-        owner: process.env.GITHUB_OWNER,
-        repo: process.env.GITHUB_REPO,
-        ref: `refs/heads/${branchName}`,
-        sha: latestCommitOnMaster.sha,
-    });
-    console.log("Response", response);
+    const issue = await createIssue();
+    console.log(issue);
     console.log("Branch created !!!");
 }
 
